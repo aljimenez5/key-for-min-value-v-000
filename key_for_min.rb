606371d2abb4acddb6db13870bc9d7e6b21323af
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 22:57:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 def key_for_min_value(name_hash)
   values_arr = []
   name_hash.collect do |name, value|
-    values_arr.push[value]
-    until values_arr[0] < value
-      values_arr.slice![0]
+    next if values_arr[0] < value
+      values_arr << value
+    else 
     end
   end
   values_arr
