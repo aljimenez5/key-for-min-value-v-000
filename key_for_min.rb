c7e3d741810ccf3570459343e01c9f1814efe67c
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 22:33:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  values_arr = []
+  values_arr = ""
   name_hash.collect do |name, value| 
     values_arr.push[name]
     if value > name_hash[values_arr[0]] 
